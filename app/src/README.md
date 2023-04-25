6191e831cc829277c9e7c696950c49a2d7b55e7d
Sigurd <sigurduhre@gmail.com>
2023-04-25 15:21:03 +0200

    done1.1

--- app/src/README.md
+++ app/src/README.md
@@ -2,3 +2,10 @@
 
 Install OpenSSL
 - Run: §sudo apt-get install openssl
+
+
+
+
+# Run server
+
+Run csano VM at: https://51.144.238.0:8088/
--- app/src/app.py
+++ app/src/app.py
@@ -12,6 +12,7 @@ app = Flask(__name__)
 app.secret_key = b'8852475abf1dcc3c2769f54d0ad64a8b7d9c3a8aa8f35ac4eb7454473a5e454c'
 
 # Define constants
+
 PASSWORDFILE = 'passwords'
 PASSWORDFILEDELIMITER = ":"
 
@@ -20,15 +21,16 @@ if not os.path.exists(PASSWORDFILE):
     open(PASSWORDFILE, 'w').close()
 
 # Define rate limiting and IP blocking
-limiter = Limiter(key_func=get_remote_address, app=app, default_limits=["3 per hour"])
-
+limiter = Limiter(key_func=get_remote_address, app=app, default_limits=["10 per hour"])
+ 
 
 
 
 # Define the route for the home page
 @app.route('/')
-@limiter.limit("5 per minute")
+@limiter.limit("3 per minute")
 def home():
+    # If the user is logged in, render the home page with the username
     if 'username' in session:
         return render_template('home.html', username=session['username'])
     else:
@@ -36,16 +38,17 @@ def home():
 
 # Define the route for the registration page
 @app.route('/register', methods=['GET'])
-@limiter.limit("5 per minute")
+@limiter.limit("3 per minute")
 def register_get():
     return render_template('register.html')
 
 # Define the route for submitting the registration form
 @app.route('/register', methods=['POST'])
-@limiter.limit("5 per minute")
+@limiter.limit("3 per minute")
 def register_post():
     # Get the username and password from the registration form
-    username = request.form['username']
+
+    username = request.form['username'] # 
     password = request.form['password']
 
     # If the username or password is empty, flash an error message and redirect back to the registration page
@@ -53,46 +56,63 @@ def register_post():
         flash('Please enter a username and password.')
         return redirect(url_for('register_get'))
     
-    # Hash the password and write it to the password file
+   
+            
+    # salted and hashed password and write it to the password file
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     with open(PASSWORDFILE, 'a') as f:
-        f.write(f'{username}{PASSWORDFILEDELIMITER}{hashed_password.decode("utf-8")}\n')
+        f.write(f'{username}{PASSWORDFILEDELIMITER}{hashed_password.decode("utf-8")}\n') 
 
     # Flash a success message and redirect to the login page
     flash('Registration successful! Please log in.')
-    return redirect(url_for('login_get'))
+    return redirect(url_for('login_get')) 
 
 # Define the route for the login page
 @app.route('/login', methods=['GET'])
-@limiter.limit("5 per minute")
+@limiter.limit("3 per minute")
 def login_get():
     return render_template('login.html')
 
 
 # Define the route for submitting the login form
 @app.route('/login', methods=['POST'])
-@limiter.limit("5 per minute")
+@limiter.limit("3 per minute")
 def login_post():
+    # Get the username and password from the login form
     username = request.form['username']
     password = request.form['password']
 
     # Check if the username and password match a record in the password file
+    # with open(PASSWORDFILE, 'r') as f:
+    #     for line in f:
+    #         stored_username, stored_password = line.strip().split(PASSWORDFILEDELIMITER) 
+            
+    #         if username == stored_username and bcrypt.checkpw(password.encode('utf-8'), stored_password.encode('utf-8')):
+
+    #             # If the username and password match, store the username in the session and redirect to the logged-in page
+    #             session['username'] = username
+    #             return redirect(url_for('loggedin'))
+
+    # Check if the username and password match a record in the password file
     with open(PASSWORDFILE, 'r') as f:
         for line in f:
-            stored_username, stored_password = line.strip().split(PASSWORDFILEDELIMITER)
+            line = line.strip()
+            if not line:
+                continue
+            stored_username, stored_password = line.split(PASSWORDFILEDELIMITER)
             if username == stored_username and bcrypt.checkpw(password.encode('utf-8'), stored_password.encode('utf-8')):
-
                 # If the username and password match, store the username in the session and redirect to the logged-in page
                 session['username'] = username
                 return redirect(url_for('loggedin'))
+
             
-    # If the username and password don't match, flash an error message and redirect back to the login page
+    # If the username and password dont match, flash an error message and redirect back to the login page
     flash('Incorrect username or password.')
     return redirect(url_for('login_get'))
 
-# Define the route for the logged-in page
+# define the route for the loggedin page
 @app.route('/loggedin')
-@limiter.limit("5 per minute")
+@limiter.limit("3 per minute")
 def loggedin():
     if 'username' in session:
         return render_template('loggedin.html', username=session['username'])
@@ -100,13 +120,16 @@ def loggedin():
         return redirect(url_for('login_get'))
 
 if __name__ == '__main__':
-    # Obtain the certificate and private key from Let's Encrypt
+    # obtain the certificate and private key from Let's Encrypt
     cert_path = 'cert.pem'
     key_path = 'key.pem'
-    cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, open(cert_path, 'rb').read())
-    key = OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM, open(key_path, 'rb').read())
 
-    # Create an SSLContext object using the certificate and private key
-    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    ssl_context.load_cert_chain(certfile=cert_path, keyfile=key_path)
-    app.run(host='0.0.0.0', port=8088, debug=True, ssl_context=ssl_context)
+    
+    # load the certificate and private key on the server
+    cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, open(cert_path, 'rb').read()) #
+    key = OpenSSL.crypto.load_privatekey(OpenSSL.crypto.FILETYPE_PEM, open(key_path, 'rb').read()) # 
+
+    # create an SSLContext object using the certificate and private key
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2) # specify the protocol TLSv1.2 also known as TLS 1.2 and HTTPS
+    ssl_context.load_cert_chain(certfile=cert_path, keyfile=key_path) #specify the certificate and private key
+    app.run(host='0.0.0.0', port=8087, debug=True, ssl_context=ssl_context)
